677dc7d83c00d172177ccebeeec6c49555d8e1cb
Sean <spmiller41@gmail.com>
2024-12-31 16:21:08 -0500

    Initial commit

---
+++ src/main/java/com/saleset/SalesetApplication.java
@@ -0,0 +1,13 @@
+package com.saleset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saleset/SalesetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saleset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
