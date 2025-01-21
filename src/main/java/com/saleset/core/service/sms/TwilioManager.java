06b09cfe1779c058cc7b375d1dff7f353b365c76
Sean <spmiller41@gmail.com>
2025-01-21 08:47:23 -0500

    Commit update to twilio manager lookup method. Will now return an invalid number as an enum type

--- src/main/java/com/saleset/core/service/sms/TwilioManager.java
+++ src/main/java/com/saleset/core/service/sms/TwilioManager.java
@@ -5,7 +5,9 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.rest.lookups.v2.PhoneNumber;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TwilioManager {
 
     @Value("${twilio.account.sid}")
@@ -13,12 +15,6 @@ public class TwilioManager {
 
     @Value("${twilio.auth.token}")
     private String authToken;
-
-    @Value("${webhook-relay.endpoint.url}")
-    private String webhookRelayURL;
-
-    @Value("${twilio.outbound.call.handler}")
-    private String outboundCallHandler;
 
     public Message sendSMS(String from, String to, String messageBody) {
         Twilio.init(accountSid, authToken);
@@ -48,12 +44,12 @@ public class TwilioManager {
 
             // Return null if number is not valid or essential data is missing
             if (!number.getValid() || number.getLineTypeIntelligence() == null ||
-                    number.getLineTypeIntelligence().get("type") == null) return null;
+                    number.getLineTypeIntelligence().get("type") == null) return new PhoneNumberDetails(PhoneLineType.INVALID);
 
             PhoneLineType lineType = categorizeLineType(number.getLineTypeIntelligence().get("type").toString());
             return new PhoneNumberDetails(lineType);
         } catch (com.twilio.exception.ApiException twilioApiException) {
-            if (twilioApiException.getStatusCode() == 404) return null;
+            if (twilioApiException.getStatusCode() == 404) return new PhoneNumberDetails(PhoneLineType.INVALID);
             else throw twilioApiException;
         }
     }
