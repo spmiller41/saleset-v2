1ebf8ff476982ffe8a22f279d47ba0456c8edb35
Sean <spmiller41@gmail.com>
2025-01-21 08:47:55 -0500

    Commit feature to handle leads at the persistance layer

--- src/main/java/com/saleset/core/service/transaction/LeadTransactionManager.java
+++ src/main/java/com/saleset/core/service/transaction/LeadTransactionManager.java
@@ -29,4 +29,12 @@ public interface LeadTransactionManager {
         );
     }
 
+    default boolean leadAddressIsValid(LeadDataTransfer leadData) {
+        boolean isValid = leadData.getStreet() != null && leadData.getZipCode() != null;
+        if (isValid) return true;
+
+        // Fallback
+        return leadData.getStreet() != null && leadData.getCity() != null && leadData.getState() != null;
+    }
+
 }
