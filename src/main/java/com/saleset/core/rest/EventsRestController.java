5f0dde5e65fe14090f80c3a96735f97729bafc29
spmiller41 <spmiller41@gmail.com>
2025-03-12 15:51:44 -0400

    Commit update for testing purposes. Testing redirection from server.

--- src/main/java/com/saleset/core/rest/EventsRestController.java
+++ src/main/java/com/saleset/core/rest/EventsRestController.java
@@ -3,8 +3,12 @@ package com.saleset.core.rest;
 import com.saleset.core.dto.EventDataTransfer;
 import com.saleset.core.entities.Event;
 import com.saleset.core.service.transaction.EventTransactionManager;
+import com.saleset.core.util.QueryUrlGenerator;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
@@ -18,6 +22,9 @@ public class EventsRestController {
     @Autowired
     private EventTransactionManager eventTransactionManager;
 
+    @Autowired
+    private QueryUrlGenerator queryUrlGenerator;
+
     @PostMapping("/email_event")
     public void emailEvent(@RequestBody List<EventDataTransfer> eventDataList) {
         eventDataList.forEach(eventData -> {
@@ -27,11 +34,25 @@ public class EventsRestController {
 
     // Anonymous endpoint name for click tracking - to be masked in booking link
     @GetMapping("/go")
-    public void smsEvent(@RequestParam("UUID") String leadUUID) {
+    public ResponseEntity<Void> smsEvent(@RequestParam("UUID") String leadUUID,
+                         @RequestParam("FNAME") String firstName,
+                         @RequestParam("LNAME") String lastName,
+                         @RequestParam("EMAIL") String email,
+                         @RequestParam("PHONE_NUMBER") String phone) {
         EventDataTransfer eventData = new EventDataTransfer();
         eventData.setEvent("click");
         eventData.setLeadUUID(leadUUID);
         System.out.println(eventData);
+        System.out.printf("First Name: %s%nLast Name: %s%nEmail: %s%nPhone: %s%n",
+                firstName, lastName, email, phone);
+
+        String bookingUrl = queryUrlGenerator.buildBooking(leadUUID, firstName, lastName, email, phone);
+        System.out.println(bookingUrl);
+
+        // Return HTTP 302 redirect to the booking URL
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header(HttpHeaders.LOCATION, bookingUrl)
+                .build();
     }
 
 }
