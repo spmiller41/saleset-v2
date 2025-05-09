ad934ded2f754869982dc304145a5f5d9cd0839e
spmiller41 <spmiller41@gmail.com>
2025-05-09 16:00:07 -0400

    Convert TaskConfig to @Component for proper config binding

--- src/main/java/com/saleset/core/service/outreach/task/TaskConfig.java
+++ src/main/java/com/saleset/core/service/outreach/task/TaskConfig.java
@@ -1,9 +1,9 @@
 package com.saleset.core.service.outreach.task;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 public class TaskConfig {
 
     @Value("${task.followUpWindowMinutes}")
