21daa3341f24a07e631777b802bda902e1e0030a
spmiller41 <spmiller41@gmail.com>
2025-05-12 12:56:23 -0400

    Add scheduling for task outreach

--- src/main/java/com/saleset/core/service/outreach/task/LeadOutreachTask.java
+++ src/main/java/com/saleset/core/service/outreach/task/LeadOutreachTask.java
@@ -1,21 +1,43 @@
 package com.saleset.core.service.outreach.task;
 
-import com.saleset.core.dao.LeadRepo;
+import com.saleset.core.entities.Lead;
 import com.saleset.core.service.outreach.Dispatcher;
+import com.saleset.core.service.transaction.leads.LeadEngagementManager;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class LeadOutreachTask {
 
     private final Dispatcher dispatcher;
-    private final LeadRepo leadRepo;
+    private final LeadEngagementManager engagementManager;
+    private final TaskConfig taskConfig;
+    private final ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
 
     @Autowired
-    public LeadOutreachTask(Dispatcher dispatcher, LeadRepo leadRepo) {
+    public LeadOutreachTask(Dispatcher dispatcher, LeadEngagementManager engagementManager, TaskConfig taskConfig) {
         this.dispatcher = dispatcher;
-        this.leadRepo = leadRepo;
+        this.engagementManager = engagementManager;
+        this.taskConfig = taskConfig;
     }
 
+    @PostConstruct
+    public void init() {
+        scheduler.initialize();
+        scheduler.scheduleAtFixedRate(this::runOutreachTask, Duration.ofMillis(taskConfig.getPollingIntervalMillis()));
+    }
+
+    private void runOutreachTask() {
+        System.out.println("Task Executed: " + LocalDateTime.now());
+
+        List<Lead> leadList = engagementManager.scanForFollowUpLeads(taskConfig.getFollowUpWindowMinutes());
+        leadList.forEach(dispatcher::executeSmsFollowUp);
+    }
 
 }
