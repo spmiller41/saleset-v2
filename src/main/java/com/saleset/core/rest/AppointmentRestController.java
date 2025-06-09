639378c4edadd90b7826ce5b60226f12c3113392
spmiller41 <spmiller41@gmail.com>
2025-06-09 15:27:06 -0400

    Add use of appointment manager for insert

--- src/main/java/com/saleset/core/rest/AppointmentRestController.java
+++ src/main/java/com/saleset/core/rest/AppointmentRestController.java
@@ -1,9 +1,7 @@
 package com.saleset.core.rest;
 
-import com.saleset.core.dao.AppointmentRepo;
-import com.saleset.core.dao.LeadRepo;
 import com.saleset.core.dto.AppointmentDataTransfer;
-import com.saleset.core.entities.Appointment;
+import com.saleset.core.service.transaction.AppointmentTransactionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,21 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppointmentRestController {
 
     @Autowired
-    private LeadRepo leadRepo;
-
-    @Autowired
-    private AppointmentRepo appointmentRepo;
+    private AppointmentTransactionManager appointmentTransactionManager;
 
     @PostMapping("/create_appointment")
     public void createAppointment(@RequestBody AppointmentDataTransfer appointmentData) {
-        System.out.println("Appointment Set. Data: " + appointmentData);
-
-        leadRepo.findLeadByUUID(appointmentData.getLeadBookingUUID()).ifPresent(lead -> {
-            Appointment appointment = new Appointment(appointmentData, lead);
-            appointmentRepo.safeInsert(appointment).ifPresent(newAppointment -> {
-                System.out.println("Appointment Inserted Successfully: " + newAppointment);
-            });
-        });
+        appointmentTransactionManager.createAppointmentForLeadUuid(appointmentData);
     }
 
 }
