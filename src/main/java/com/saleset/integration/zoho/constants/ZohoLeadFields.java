a3f4621ade10d7fb15f1cfba39cf30b69f8e5a46
spmiller41 <spmiller41@gmail.com>
2025-06-12 14:26:29 -0400

    Add additional Zoho fieldss

--- src/main/java/com/saleset/integration/zoho/constants/ZohoLeadFields.java
+++ src/main/java/com/saleset/integration/zoho/constants/ZohoLeadFields.java
@@ -2,8 +2,24 @@ package com.saleset.integration.zoho.constants;
 
 public class ZohoLeadFields {
 
+    // Most Zoho CRM requests require nesting inside a 'data' array
     public static final String DATA = "data";
+
     public static final String APPOINTMENT = "Appointment";
     public static final String OWNER = "Owner";
+    public static final String PRODUCT1 = "Product1";
+    public static final String DESCRIPTION = "Description";
+    public static final String LEAD_SOURCE = "Lead_Source";
+    public static final String SUB_SOURCE = "Sub_Source";
+    public static final String AMBASSADOR_PROMOTER = "Ambassador_Promoter";
+    public static final String FIRST_NAME = "First_Name";
+    public static final String LAST_NAME = "Last_Name";
+    public static final String EMAIL = "Email";
+    public static final String PHONE = "Phone";
+    public static final String MOBILE = "Mobile";
+    public static final String STREET = "Street";
+    public static final String CITY = "City";
+    public static final String STATE = "State";
+    public static final String ZIP_CODE = "Zip_Code";
 
 }
