c26a2ac8fefdc10f62166b6b41131999de035d90
Sean <spmiller41@gmail.com>
2025-06-16 19:56:07 -0400

    Add additional update

--- src/main/java/com/saleset/SalesetApplication.java
+++ src/main/java/com/saleset/SalesetApplication.java
@@ -1,11 +1,21 @@
 package com.saleset;
 
+import com.saleset.core.dao.LeadRepo;
 import com.saleset.core.dto.request.AppointmentRequest;
+import com.saleset.core.entities.Appointment;
+import com.saleset.core.entities.Lead;
+import com.saleset.integration.zoho.dto.response.ZohoLeadCreateUpdateResponse;
+import com.saleset.integration.zoho.dto.response.ZohoLeadFetchResponse;
+import com.saleset.integration.zoho.service.ZohoLeadsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @SpringBootApplication
 @EnableScheduling
@@ -23,3 +33,4 @@ public class SalesetApplication {
 	*/
 
 }
+
