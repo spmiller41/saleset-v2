b1897d3ba4ba6c4e5ec01078243db6e2e45a5b37
Sean <spmiller41@gmail.com>
2025-06-17 03:49:30 -0400

    Add update to commandline runner for testing

--- src/main/java/com/saleset/SalesetApplication.java
+++ src/main/java/com/saleset/SalesetApplication.java
@@ -1,10 +1,17 @@
 package com.saleset;
 
+import com.saleset.core.dao.AddressRepo;
+import com.saleset.core.dao.AppointmentRepo;
 import com.saleset.core.dao.LeadRepo;
 import com.saleset.core.dto.request.AppointmentRequest;
+import com.saleset.core.dto.request.LeadRequest;
+import com.saleset.core.entities.Address;
 import com.saleset.core.entities.Appointment;
 import com.saleset.core.entities.Lead;
-import com.saleset.integration.zoho.dto.response.ZohoLeadCreateUpdateResponse;
+import com.saleset.core.enums.LeadStage;
+import com.saleset.core.service.persistence.leads.LeadEntryPipelineManager;
+import com.saleset.integration.zoho.constants.ZohoLeadFields;
+import com.saleset.integration.zoho.dto.response.ZohoLeadCreateResponse;
 import com.saleset.integration.zoho.dto.response.ZohoLeadFetchResponse;
 import com.saleset.integration.zoho.service.ZohoLeadsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,11 +33,82 @@ public class SalesetApplication {
 	}
 
 	/*
+	@Autowired
+	private ZohoLeadsService zohoLeadService;
+
+	@Autowired
+	private LeadRepo leadRepo;
+
+	@Autowired
+	private AppointmentRepo appointmentRepo;
+
+	@Autowired
+	private AddressRepo addressRepo;
+
+	@Autowired
+	private LeadEntryPipelineManager entryPipeline;
+
 	@Bean
 	public CommandLineRunner demo() {
-		return (args) -> {};
+		return (args) -> {
+
+			AppointmentRequest testRequest = new AppointmentRequest();
+			testRequest.setStartDateTime(LocalDateTime.now().plusDays(5));
+			testRequest.setAppointmentType("Virtual Meeting");
+			testRequest.setFirstName("Sean");
+			testRequest.setLastName("Te$t");
+			testRequest.setEmail("sean.tester@example.com");
+			testRequest.setPhone("+16318895508");
+			testRequest.setStreet("123 Test St");
+			testRequest.setCity("Fake City");
+			testRequest.setState("NY");
+			testRequest.setZip("11980");
+
+			ZohoLeadCreateResponse response = zohoLeadService.createLead(testRequest);
+
+			if (response.getResponseCode().equals("DUPLICATE_DATA")) {
+				Optional<Lead> optLead = leadRepo.findLeadByExternalId(response.getZohoLeadId());
+
+				if (optLead.isPresent()) {
+					Lead lead = optLead.get();
+					lead.setCurrentStage(LeadStage.CONVERTED.toString());
+					leadRepo.safeUpdate(lead);
+
+					Address address = new Address(testRequest);
+
+					Appointment appointment = new Appointment(testRequest, lead);
+					appointmentRepo.safeInsert(appointment);
+
+					zohoLeadService.updateLeadAppointment(appointment, address, lead.getZcrmExternalId());
+				} else {
+					zohoLeadService.fetchLead(response.getZohoLeadId()).ifPresent(fetchedZohoLead -> {
+						LeadRequest leadData = new LeadRequest(
+								testRequest,
+								fetchedZohoLead.getId(),
+								fetchedZohoLead.getAutoNumber(),
+								ZohoLeadFields.LEAD_SOURCE_DEFAULT_VALUE,
+								ZohoLeadFields.SUB_SOURCE_DEFAULT_VALUE,
+								LeadStage.CONVERTED.toString());
+
+						entryPipeline.manageLead(leadData);
+
+						leadRepo.findLeadByExternalId(fetchedZohoLead.getId()).ifPresent(lead -> {
+							Appointment appointment = new Appointment(testRequest, lead);
+							appointmentRepo.safeInsert(appointment);
+
+							addressRepo.findAddressById(lead.getId()).ifPresent(address -> {
+								zohoLeadService.updateLeadAppointment(appointment, address, leadData.getZcrmExternalId());
+							});
+						});
+					});
+				}
+			}
+
+
+		};
 	}
 	*/
 
 }
+
 
