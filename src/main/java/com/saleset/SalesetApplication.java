a40dd706f8335eda84799dad631dd51eb83aa57b
spmiller41 <spmiller41@gmail.com>
2025-06-20 15:40:38 -0400

    Update commandline runner

--- src/main/java/com/saleset/SalesetApplication.java
+++ src/main/java/com/saleset/SalesetApplication.java
@@ -1,7 +1,9 @@
 package com.saleset;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -13,36 +15,10 @@ public class SalesetApplication {
 	}
 
 	/*
-
-	@Autowired
-	private ExternalLeadAppointmentHandler externalLeadAppointmentHandler;
-
 	@Bean
-	public CommandLineRunner demo() {
-		return (args) -> {
-
-			AppointmentRequest testRequest = new AppointmentRequest();
-			testRequest.setStartDateTime(LocalDateTime.now().plusDays(10));
-			testRequest.setEndDateTime(LocalDateTime.now().plusDays(10).plusMinutes(60));
-			testRequest.setAppointmentType("Virtual Meeting");
-			testRequest.setFirstName("Sean");
-			testRequest.setLastName("Te$t");
-			testRequest.setEmail("sean.tester@example.com");
-			testRequest.setPhone("+16318895508");
-			testRequest.setStreet("333 Test St");
-			testRequest.setCity("Test City");
-			testRequest.setState("NY");
-			testRequest.setZip("11980");
-			testRequest.setBookingReference("bookingReference_1");
-			testRequest.setBookingSource("Booking_Link");
-
-			externalLeadAppointmentHandler.handleExternalAppointment(testRequest);
-
-		};
-	}
-
+	public CommandLineRunner demo() { return (args) -> {}; }
 	*/
-
+	
 }
 
 
