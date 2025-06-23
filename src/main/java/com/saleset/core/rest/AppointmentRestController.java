eae134c6a1f255eb7264d061c0278c24f397b570
spmiller41 <spmiller41@gmail.com>
2025-06-23 14:49:29 -0400

    Add use of internal appointment handler

--- src/main/java/com/saleset/core/rest/AppointmentRestController.java
+++ src/main/java/com/saleset/core/rest/AppointmentRestController.java
@@ -1,8 +1,7 @@
 package com.saleset.core.rest;
 
 import com.saleset.core.dto.request.AppointmentRequest;
-import com.saleset.core.service.persistence.AppointmentTransactionManager;
-import com.saleset.integration.zoho.service.ZohoLeadsService;
+import com.saleset.usecase.InternalLeadAppointmentHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,17 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppointmentRestController {
 
     @Autowired
-    private AppointmentTransactionManager appointmentTransactionManager;
-
-    @Autowired
-    private ZohoLeadsService zohoLeadsService;
+    private InternalLeadAppointmentHandler internalLeadAppointmentHandler;
 
     @PostMapping("/create_appointment")
     public void createAppointment(@RequestBody AppointmentRequest appointmentData) {
-        appointmentTransactionManager.scheduleAppointmentForExistingLead(appointmentData)
-                .ifPresent(appointmentResult -> {
-            zohoLeadsService.updateLeadAppointment(appointmentResult.appointment(), appointmentResult.lead());
-        });
+        internalLeadAppointmentHandler.handleInternalAppointment(appointmentData);
     }
 
 }
