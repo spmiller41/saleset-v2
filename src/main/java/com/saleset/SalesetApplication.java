055a33cc786b7ecd4c6f186804405389f2cdb9e0
spmiller41 <spmiller41@gmail.com>
2025-06-24 14:43:47 -0400

    Update commandline runner

--- src/main/java/com/saleset/SalesetApplication.java
+++ src/main/java/com/saleset/SalesetApplication.java
@@ -1,10 +1,20 @@
 package com.saleset;
 
+import com.saleset.core.dao.LeadRepo;
+import com.saleset.core.dto.request.LeadRequest;
+import com.saleset.core.entities.Appointment;
+import com.saleset.core.entities.Lead;
+import com.saleset.core.service.persistence.leads.LeadEntryPipelineManager;
+import com.saleset.integration.zoho.service.ZohoDealsService;
+import com.saleset.integration.zoho.service.ZohoLeadsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 @EnableScheduling
@@ -15,9 +25,58 @@ public class SalesetApplication {
 	}
 
 	/*
+	@Autowired
+	private LeadEntryPipelineManager leadEntryPipelineManager;
+
+	@Autowired
+	private ZohoLeadsService zohoLeadsService;
+
+	@Autowired
+	private ZohoDealsService zohoDealsService;
+
+	@Autowired
+	private LeadRepo leadRepo;
+
 	@Bean
-	public CommandLineRunner demo() { return (args) -> {}; }
+	public CommandLineRunner demo() {
+
+		return (args) -> {
+
+
+			LeadRequest leadRequest = new LeadRequest();
+			leadRequest.setFirstName("Sean");
+			leadRequest.setLastName("Te$t");
+			leadRequest.setStreet("254 Dogwood Road West");
+			leadRequest.setCity("Mastic Beach");
+			leadRequest.setState("NY");
+			leadRequest.setZipCode("11951");
+			leadRequest.setLeadSource("Internet PPC");
+			leadRequest.setSubSource("Google");
+			leadRequest.setPrimaryPhone("6318895508");
+			leadRequest.setEmail("seantesting@test.com");
+			leadRequest.setStage("New");
+			leadRequest.setZcrmExternalId("3880966000334300145");
+			leadRequest.setZcrmAutoNumber("90569");
+
+
+
+			Appointment appointment = new Appointment();
+			appointment.setAppointmentType("Virtual Meeting");
+			appointment.setBookingSource("YCBM");
+			appointment.setBookingReference("ycbm-test-01");
+			appointment.setStartDateTime(LocalDateTime.now().plusDays(5));
+
+			leadRepo.findLeadById(1).ifPresent(lead -> {
+				zohoLeadsService.updateLeadAppointment(appointment, lead);
+			});
+
+
+
+		};
+
+	}
 	*/
+
 	
 }
 
